86e0e556e7780910cd5612cde48afdcd67e85267
Robert E Fry <inbox@robertfry.xyz>
2022-06-22 22:01:42 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+target="/usr/share/fcitx5/themes/"
+
+install -Dm 644 {.,"$target"}/Slate-Blue/theme.conf
+install -Dm 644 {.,"$target"}/Slate-Blue/arrow.png
+install -Dm 644 {.,"$target"}/Slate-Blue/highlight.png
+install -Dm 644 {.,"$target"}/Slate-Blue/panel.png
+install -Dm 644 {.,"$target"}/Slate-Blue/radio.png
